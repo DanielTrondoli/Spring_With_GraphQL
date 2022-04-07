6406fdb09ed4708f8abb37db5840fe105034df99
Daniel Trondoli <daniel.trondoli@monitoratec.com.br>
2022-04-06 22:29:53 -0300

    add mysql_commands

--- mysql_commands.sh
+++ mysql_commands.sh
@@ -1,4 +1,7 @@
 
+# Montar Imagem do Mysql
+docker build -t compras_db .
+
 # Executar o conteiner do Mysql
 docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=myroot -e MYSQL_PASSWORD=myroot -e MYSQL_DATABASE=Compras compras_db
 
